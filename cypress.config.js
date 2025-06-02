591a6adeec43a017f36570f3a43d388d547feece
Zorin95670 <moittie.vincent@gmail.com>
2025-06-02 14:42:02 +0200

    fix: simplify launching runner

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,20 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin =
     require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin =
-    require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;1
+    require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const dotenv = require("dotenv");
+const path = require('path');
 
 dotenv.config();
+
+const absolutePath = path.resolve(process.env.CYPRESS_FEATURES_PATH);
 
 module.exports = defineConfig({
     e2e: {
         env: process.env,
-        specPattern: "**/*.feature",
+        specPattern: `${absolutePath}/**/*.feature`,
         supportFile: 'support/e2e.js',
         reporter: require.resolve('@badeball/cypress-cucumber-preprocessor/pretty-reporter'),
         async setupNodeEvents(on, config) {
